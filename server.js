6f66e3ba04c1d66cc36563c1cd1f69bb5adb391b
DPLong <dplong742@gmail.com>
2022-06-23 00:08:47 -0400

    readme and final push

--- server.js
+++ server.js
@@ -1,90 +1,46 @@
+//requires
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+//port, express init, and database require
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const notesDB = require('./db/db.json');
 
+//allows the public directory to be read
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-// app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-// app.get('/api/notes', (req, res) => res.json(notesDB));
+//get paths
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/api/notes', (req, res) => res.json(notesDB));
 
+//post path
+app.post('/api/notes', (req, res) => {
+    const dbPath = path.join(__dirname, "/db/db.json");
+    const newNote = req.body;
+    let highID = 99;
+    
+    for (let i = 0; i < notesDB.length; i++) {
+        const notes = notesDB[i];
 
-const uniqueID = () => {
-    Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-};
-
-//stuff
-
-// Setup notes variable
-fs.readFile("db/db.json", "utf8", (err, data) => {
-
-    if (err) throw err;
-
-    var notes = JSON.parse(data);
-
-    // API ROUTES
-    // ========================================================
-
-    // Setup the /api/notes get route
-    app.get("/api/notes", function (req, res) {
-        // Read the db.json file and return all saved notes as JSON.
-        res.json(notes);
-    });
-
-    // Setup the /api/notes post route
-    app.post("/api/notes", function (req, res) {
-        // Receives a new note, adds it to db.json, then returns the new note
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDb();
-        return console.log("Added new note: " + newNote.title);
-    });
-
-    // Retrieves a note with specific id
-    app.get("/api/notes/:id", function (req, res) {
-        // display json for the notes array indices of the provided id
-        res.json(notes[req.params.id]);
-    });
-
-    // Deletes a note with specific id
-    app.delete("/api/notes/:id", function (req, res) {
-        notes.splice(req.params.id, 1);
-        updateDb();
-        console.log("Deleted note with id " + req.params.id);
-    });
-
-    // VIEW ROUTES
-    // ========================================================
-
-    // Display notes.html when /notes is accessed
-    app.get('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
-    });
-
-    // Display index.html when all other routes are accessed
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
-    });
-
-    //updates the json file whenever a note is added or deleted
-    function updateDb() {
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-            return true;
-        });
+        if (notes.id > highID) highID = notes.id;
     }
 
+    newNote.id = highID + 1;
+    notesDB.push(newNote);
+
+    fs.writeFile(dbPath, JSON.stringify(notesDB, null, 2), (err) => {
+        if (err) console.log(err);
+        console.log("Your note was saved!");
+    });
+    res.json(newNote);
 });
 
+//logs a message to let the user know the app is listening
 app.listen(PORT, function () {
     console.log(`App listening on http://localhost:${PORT}`);
 });
